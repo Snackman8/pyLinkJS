ccca6c2168139562f9040a0b5d49f9e102ac8f09
Snackman8 <lawy888@gmail.com>
2022-11-09 18:11:45 -0800

    prepare for upload to pypi

--- setup.py
+++ setup.py
@@ -1,12 +1,29 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+import os
+from setuptools import setup
+
+def get_version():
+    f = open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'pylinkjs/__init__.py'), 'r')
+    s = f.read()
+    f.close()
+    for line in s.split('\n'):
+        if line.startswith('__version__'):
+            version = line.partition('=')[2].strip()
+            if version[0] == '"':
+                version = version.split('"')[1]
+            if version[0] == "'":
+                version = version.split("'")[1]
+            break
+    return version
 
 setup(name='pylinkjs',
-      version='1.0',
+      version=get_version(),
       description='Python Javascript Bridge',
       author='Lawrence Yu',
       author_email='lawy888@gmail.com',
       url='https://github.com/Snackman8/pyLinkJS',
       packages=['pylinkjs', 'pylinkjs.plugins'],
-      include_package_data=True)
+      include_package_data=True,
+      install_requires=["tornado", ],
+      )
