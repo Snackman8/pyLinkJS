1908e3c08736607deb70f63a563e4f23769dd492
Lawrence Yu <l.yu@commercecasino.net>
2024-08-13 15:13:28 -0700

    update selenium testing

--- pylinkjs/utils/testing.py
+++ pylinkjs/utils/testing.py
@@ -6,7 +6,7 @@ import os
 import time
 import inspect
 from io import BytesIO
-from PIL import Image, ImageDraw
+from PIL import Image, ImageChops, ImageDraw
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import socket
@@ -25,19 +25,21 @@ def _find_free_port():
         return s.getsockname()[1]
 
 
-def _start_pylinkjs_app(server_func, url):
+def _start_pylinkjs_app(server_func, url, server_func_args):
     """ start a pylink js app in a separate thread using a new unused port
 
         Args:
             server_func - function to call to start the application, i.e. main
             url - url to call to access the started application, i.e. http://localhost
+            server_func_args - arguments to pass into the function
 
         Returns:
             tuple of thread running the application and url including port number
      """
     # start server with free port
     port = _find_free_port()
-    t = Thread(target=server_func, args=({'port': port},))
+    server_func_args['port'] = port
+    t = Thread(target=server_func, args=(server_func_args,))
     t.daemon = True
     t.start()
 
@@ -66,6 +68,9 @@ def _perform_comparison(truth_path, update_truth, png):
     test_filepath = os.path.join(
         os.path.abspath(os.path.dirname(truth_path)), f"test_{func_name}.png"
     )
+    xor_filepath = os.path.join(
+        os.path.abspath(os.path.dirname(truth_path)), f"test_{func_name}_xor.png"
+    )
 
     # write truth if needed
     if update_truth:
@@ -80,15 +85,23 @@ def _perform_comparison(truth_path, update_truth, png):
         truth_png = b''
 
     # perform comparison
-    same = truth_png == png
-    if not same:
+    im1 = Image.open(BytesIO(png))
+    im2 = Image.open(BytesIO(truth_png))
+    im_xor = ImageChops.difference(im1, im2)
+
+    # perform comparison
+    same = im_xor.getbbox()
+    if same is not None:
         with open(test_filepath, "wb") as f:
             f.write(png)
+        im_xor.save(xor_filepath, format='PNG')
     else:
         if os.path.exists(test_filepath):
             os.remove(test_filepath)
+        if os.path.exists(xor_filepath):
+            os.remove(xor_filepath)
 
-    assert same
+    assert same is None
 
 
 def _process_redacted(png, redacted):
@@ -120,13 +133,14 @@ def _process_redacted(png, redacted):
 # --------------------------------------------------
 #    Functions
 # --------------------------------------------------
-def download_and_compare(server_func, url, update_truth=False, truth_path='.', print_extra_delay=1, print_timeout=5, redacted=[]):
+def download_and_compare(server_func, url, server_func_args={}, update_truth=False, truth_path='.', print_extra_delay=1, print_timeout=5, redacted=[]):
     """
         run an application and download the png of the finished application.  Compare with truth
 
         Args:
             server_func - function to run to start application
             url - url to call for testing, i.e. http://localhost
+            server_func_args - dictionary of arguments to pass into the server function
             update_truth - if set to True, the truth file will automatically be updated
             truth_path - path to look for truth files
             print_extra_delay - how many seconds to wait after printing is complete before capturing png
@@ -134,7 +148,7 @@ def download_and_compare(server_func, url, update_truth=False, truth_path='.', p
             redacted - list of rectangle coordinates (x, y, w, h) to redact, i.e. [[0, 0, 50, 100], [200, 200, 10, 100]]
     """
     # start the app
-    _t, url = _start_pylinkjs_app(server_func, url)
+    _t, url = _start_pylinkjs_app(server_func, url, server_func_args)
 
     # add the output type of png
     if '?' in url:
@@ -144,7 +158,6 @@ def download_and_compare(server_func, url, update_truth=False, truth_path='.', p
 
     # render the url
     time.sleep(1)
-    print(url)
     img = urllib.request.urlopen(url)
     png = img.read()
 
@@ -155,17 +168,27 @@ def download_and_compare(server_func, url, update_truth=False, truth_path='.', p
     _perform_comparison(truth_path, update_truth, png)
 
 
-def selenium_test_begin(server_func, url):
+def selenium_test_begin(server_func, url, server_func_args={}, browser_options=[]):
     # start the app
-    _t, url_with_port = _start_pylinkjs_app(server_func, url)
+    _t, url_with_port = _start_pylinkjs_app(server_func, url, server_func_args)
 
     # start selenium
     chrome_options = Options()
-    chrome_options.add_argument("--headless=new") # for Chrome >= 109
+
+    headless_present = False
+    for opt in browser_options:
+        if opt ==  '--headless=no':
+            headless_present = True
+        else:
+            chrome_options.add_argument(opt)
+    if not headless_present:
+        chrome_options.add_argument("--headless=new") # for Chrome >= 109
     driver = webdriver.Chrome(options=chrome_options)
-    driver.get(url_with_port)    
+    driver.get(url_with_port)
+    time.sleep(1)
 
     return driver, url_with_port
+
 
 def selenium_test_compare_and_finish(driver, update_truth=False, truth_path='.', redacted=[], sleep_time=1):
     # get screenshot
@@ -178,42 +201,3 @@ def selenium_test_compare_and_finish(driver, update_truth=False, truth_path='.',
 
     # perform comparison
     _perform_comparison(truth_path, update_truth, png)
-
-# def download_and_compare_selenium(server_func, url, exercise_func, update_truth=False, truth_path='.', redacted=[]):
-#     """
-#         run an application and download the png of the finished application.  Exercise using selenium, compare with truth
-#
-#         Args:
-#             server_func - function to run to start application
-#             url - url to call for testing, i.e. http://localhost
-#             exercise_func - function to call to exercise the application using selenium.  prototype is f(driver, url)
-#             update_truth - if set to True, the truth file will automatically be updated
-#             truth_path - path to look for truth files
-#             redacted - list of rectangle coordinates (x, y, w, h) to redact, i.e. [[0, 0, 50, 100], [200, 200, 10, 100]]
-#     """
-#     # setup for test
-#     driver, url_with_port = selenium_test_begin(server_func, url)
-#
-#     # # start the app
-#     # _t, url = _start_pylinkjs_app(server_func, url)
-#     #
-#     # # start selenium
-#     # chrome_options = Options()
-#     # chrome_options.add_argument("--headless=new") # for Chrome >= 109
-#     # driver = webdriver.Chrome(options=chrome_options)
-#
-#     # exercise the app
-#     driver.get(url_with_port)
-#     exercise_func(driver, url)
-#
-#     selenium_test_compare_and_finish(driver, update_truth, truth_path, redacted)
-#     #
-#     # # get screenshot
-#     # png = driver.get_screenshot_as_png()
-#     # driver.quit()
-#     #
-#     # # process redacted sections
-#     # png = _process_redacted(png, redacted)
-#     #
-#     # # perform comparison
-#     # _perform_comparison(truth_path, update_truth, png)
